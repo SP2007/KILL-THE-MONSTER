67c45ea35996cf2a82f781919b56682e9c5f77a7
SP2007 <74725161+SP2007@users.noreply.github.com>
2021-02-03 21:31:20 +0530

    Add files via upload

---
+++ superhero.js
@@ -0,0 +1,28 @@
+class Superhero{
+    constructor(x,y,r){
+        var options = { 
+            density: 1, 
+            frictionAir: 1
+
+        };
+       
+   this.x=x;
+   this.y=y;
+   this.r=r;
+   this.image=loadImage("Superhero-01.png");
+   this.body=Bodies.circle(this.x, this.y, (this.r)/2, options);
+   World.add(world, this.body);
+
+    }
+    display() {
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        fill("red");
+        ellipse(0, 0, this.width, this.height);
+        pop();
+      }
+
+
+}
